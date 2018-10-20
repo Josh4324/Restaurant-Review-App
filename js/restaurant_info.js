a4313bfb7f20f62b7db0c62be2c0d92a2bbff655
ADESANYA JOSHUA AYODEJI <adesanyajoshua@ymail.com>
2018-10-20 23:49:02 +0100

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -196,7 +196,7 @@ getParameterByName = (name, url) => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/Restaurant-Review-App/sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
